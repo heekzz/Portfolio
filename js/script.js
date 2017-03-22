b135fde4b87a8526a737944a18be8cec446fb4e2
Fredrik Håkansson <freha309@student.liu.se>
2017-03-22 14:32:08 +0100

    Added more to previous work

--- js/script.js
+++ js/script.js
@@ -17,3 +17,40 @@ $("#current-year").html(new Date().getFullYear());
 $('.nav a').on('click', function(){
     $('.navbar-collapse').collapse('hide')
 });
+
+
+
+
+
+// GitHub -- WARNING: SYNCHRONOUS!!
+var source = $("#github-template").html();
+var template = Handlebars.compile(source);
+$("#github-placeholder").html(template());
+var repositories = [];
+$.ajax({
+    url: "https:/api.github.com/users/heekzz/repos",
+    async: false,
+    success: function (response) {
+        for (var i = 0; i < response.length; i++) {
+            var repo = {
+                name: response[i].name,
+                description: response[i].description,
+                html_url: response[i].html_url
+            };
+            repositories.push(repo);
+        }
+        var context = {repos: repositories};
+        var html = template(context);
+        $("#github-placeholder").html(html);
+    }
+});
+
+$(".expand").click(function (e) {
+    var target = $(this).data("target");
+    if ($(target).is(":visible")) {
+        $(this).html("show")
+    } else {
+        $(this).html("hide")
+    }
+    $(target).collapse("toggle");
+});
